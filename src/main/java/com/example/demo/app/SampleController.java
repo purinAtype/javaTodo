afd0a17e2afd738777388d44c27b744077d3a51c
*zgjDlete7F <k-tsuchiya@star-career.co.jp>
2023-03-25 10:44:58 +0900

    first Commit.

---
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -0,0 +1,40 @@
+package com.example.demo.app;
+
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public SampleController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@GetMapping("/test")
+	public String test(Model model) {
+		String sql = "SELECT id, name, email "
+				+ "FROM inquiry WHERE id = 1";
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+
+		String sql2 = "SELECT id, name, email "
+				+ "FROM inquiry WHERE id = 2";
+		Map<String, Object> map2 = jdbcTemplate.queryForMap(sql2);
+		
+		model.addAttribute("name2", map2.get("name"));
+		model.addAttribute("email2", map2.get("email"));
+
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
+		model.addAttribute("title", "Inquiry Form");
+		return "test";
+	}
+
+}
---
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -0,0 +1,109 @@
+package com.example.demo.app.inquiry;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+
+@Controller
+@RequestMapping("/inquiry")
+public class InquiryController {
+
+	private final InquiryService inquiryService;
+
+	public InquiryController(InquiryService inquiryService){
+		this.inquiryService = inquiryService;
+	}
+
+	@GetMapping
+	public String index(Model model) {
+		List<Inquiry> list = inquiryService.getAll();
+
+		model.addAttribute("inquiryList", list);
+		model.addAttribute("title", "Inquiry Index");
+
+		return "inquiry/index_boot";
+	}
+
+	@GetMapping("/form")
+	public String form(InquiryForm inquiryForm,
+			Model model,
+			@ModelAttribute("complete") String complete) {
+
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form_boot";
+	}
+
+	@PostMapping("/form")
+	public String formGoBack(InquiryForm inquiryForm, Model model) {
+		model.addAttribute("title", "InquiryForm");
+		return "inquiry/form_boot";
+	}
+
+
+	@PostMapping("/confirm")
+	public String confirm(@Validated InquiryForm inquiryForm,
+	        BindingResult result,
+	        Model model) {
+
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form_boot";
+		}
+		model.addAttribute("title", "確認ページ");
+		return "inquiry/confirm_boot";
+	}
+
+	@PostMapping("/complete")
+	public String complete(@Validated InquiryForm inquiryForm,
+	        BindingResult result,
+	        Model model,
+	        RedirectAttributes redirectAttributes) {
+
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form_boot";
+		}
+
+		Inquiry inquiry = new Inquiry();
+		inquiry.setName(inquiryForm.getName());
+		inquiry.setEmail(inquiryForm.getEmail());
+		inquiry.setContents(inquiryForm.getContents());
+		inquiry.setCreated(LocalDateTime.now());
+
+		if(inquiryForm.getId() == 0) {
+			inquiryService.save(inquiry);
+		} else {
+			inquiry.setId(inquiryForm.getId());
+			inquiryService.update(inquiry);
+		}
+
+		redirectAttributes.addFlashAttribute("complete", "Completed!");
+		return "redirect:/inquiry/form";
+	}
+
+	@PostMapping("/update")
+	public String update(@Validated InquiryForm inquiryForm,
+	        BindingResult result,
+	        Model model) {
+
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/index_boot";
+		}
+
+		model.addAttribute("title", "更新フォーム");
+		return "inquiry/form_boot";
+	}
+}
